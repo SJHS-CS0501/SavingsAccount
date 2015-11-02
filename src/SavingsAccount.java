a9491358f3e8d6072c232ca80b2270168dabf50c
Jonathan Woytek <jwoytek@saintjosephhs.com>
2015-11-01 21:15:32 -0500

    updated initial commit

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -8,12 +8,4 @@
  */
 public class SavingsAccount {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
---
+++ src/SavingsAccountTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author woytek
+ *
+ */
+public class SavingsAccountTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
