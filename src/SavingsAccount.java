f7ca4235e00f4fe529208cba8f67efbdf02874ea
ryluchs17 <ryluchs17@saintjosephhs.com>
2015-11-03 21:07:50 -0500

    wrote this program

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,11 +1,51 @@
 /**
+ * Class to allow creation of savings accounts
+ * @ author Ryan Luchs
  * 
  */
 
 /**
- * @author woytek
+ * This is a class for savings accounts
+ * @author Ryan Luchs
  *
  */
 public class SavingsAccount {
-
+	double balance = 0;
+	private double annualInterest = 0;
+	
+	/**
+     * Create an account with balance m
+     * @param m The initial balance
+     * @param r The annual interest rate
+     */
+	SavingsAccount(double m, double r){
+		balance = m;
+		annualInterest = r;
+	}
+	
+	/**
+     * Deposit $m into the account
+     * @param m $s to deposit
+     */
+	public void deposit(double m){
+		balance += m;
+	}
+	
+	/**
+     * Withdraw $m from the account
+     * @param m $s to withdraw
+     */
+	public void withdraw(double m){
+		balance -= m;
+	}
+	
+	/**
+     * Add monthly interest to the account's balance
+     * @return Interest made
+     */
+	public double addMonthlyInterest(){
+		double oldBalance = balance; // store old balance value
+		balance += 1 + (annualInterest/12) * balance; // calculate new balance after interest
+		return balance - oldBalance; // return amount of interest made
+	}
 }
