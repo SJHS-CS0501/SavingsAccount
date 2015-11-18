7cff66e8c2dd0e9893cd809aacc0798aecab4e65
juliannanichols <junichols17@saintjosephhs.com>
2015-11-18 08:47:23 -0500

    moved stuff around

--- src/SavingsAccountTest.java
+++ src/SavingsAccountTest.java
@@ -1,19 +1,93 @@
-/**
- * 
- */
+import java.util.Scanner;
 
 /**
+ * This program will store a savings account's annual interest rate and balance.
  * @author woytek
- *
+ * @author Julianna Nichols
  */
 public class SavingsAccountTest {
 
 	/**
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner keyboard = new Scanner( System.in ); //creates Scanner object
+		SavingsAccount test = new SavingsAccount(); //creates new SavingsAccountTest
+		double balance = 0; //user's balance
+		double annualInterest; //annual interest rate
+		int choice; //what user wants to do
+		int est; //when account was established
+		
+		System.out.print( "What is the amount of money currently in your savings account?" );
+		balance = keyboard.nextDouble();
+		
+		System.out.print( "What is the savings account's annual interest rate?" );
+		annualInterest = keyboard.nextDouble();
+		
+		System.out.print( "Do you want to:\n1. make a withdrawal.\n2. make a deposit.\n3. calculate balance including "
+						+ "monthly interest.\nType in the number corresponding to your choice" );
+		choice = keyboard.nextInt();
+		
+		switch( choice ) {
+			case 1:
+				withdrawal(balance);
+				break;
+			case 2:
+				deposit(balance);
+				break;
+			case 3:
+				monthlyInterest(balance , annualInterest);
+				break;
+		}
+	}
+	
+	/**
+	 * Making a withdrawal
+	 * @param balance before withdrawal
+	 * @return balance after withdrawal
+	 */
+	public static double withdrawal( double balance ) {
+		double withdrawal = 0;
+		Scanner keyboard = new Scanner( System.in );
+		
+		System.out.print( "How much would you like to take out?" );
+		withdrawal = keyboard.nextDouble();
+		
+		balance -= withdrawal;
+		
+		return balance;
+	}
+	
+	/**
+	 * Making a deposit
+	 * @param balance before deposit
+	 * @return balance after deposit
+	 */
+	public static double deposit( double balance ) {
+		double deposit = 0;
+		Scanner keyboard = new Scanner( System.in );
+		
+		System.out.print( "How much would you like to deposit?" );
+		deposit = keyboard.nextDouble();
+		
+		balance += deposit;
+		
+		return balance;
+	}
+	
+	/**
+	 * Calculating monthly interest and new balance after addition of monthly interest
+	 * @param balance before monthly interest is added
+	 * @return balance after monthly interest is added
+	 */
+	public static double monthlyInterest( double balance , double annualInterest ) {
+		double monthlyInterest;
+		
+		monthlyInterest =  annualInterest / 12;
+		
+		balance += monthlyInterest;
+		return balance;
 	}
 
 }
