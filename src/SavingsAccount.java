3ad76c824c98c402a386f5033bef108cec505872
jprotivnak <jprotivnak@me.com>
2015-11-19 09:38:06 -0500

    Started

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,11 +1,18 @@
 /**
- * 
- */
-
-/**
- * @author woytek
+ * @author Jack Protivnak
  *
  */
-public class SavingsAccount {
-
+public class SavingsAccount{
+	private double startBal;
+	private double interest;
+	
+	public SavingsAccount( double bal ) {
+		startBal = bal;
+	}
+	
+	public void Interest() {
+		System.out;println( "Please enter your annual interest as a percent: " );
+		interest = nextDouble();
+	}
 }
+
