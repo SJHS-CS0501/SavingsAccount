5519171980652ac24998f0b1fa16e53c1a024ca3
rydanie <rydanielson18@saintjosephhs.com>
2015-11-19 10:31:32 -0500

    fdkjfhhfd
    hfghfd

--- src/SavingsAccountTest.java
+++ src/SavingsAccountTest.java
@@ -14,6 +14,12 @@ public class SavingsAccountTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		double balance;
+		double intrest;
+		double deposite;
+		double withdraw;
+		int monthTimePeriod;
+		
 		
 
 	}
