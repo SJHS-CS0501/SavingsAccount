d504435b9effee94232c37bfac1de08cfb8dffa7
isschroeder <kittypuppybird@gmail.com>
2015-11-20 10:31:22 -0500

    Methods added

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -9,4 +11,46 @@
  */
 public class SavingsAccount {
 
+	private static Double interest;
+	private static Double bal;
+	private static Double months;
+	static Double deposit;
+	static Double withdrawal;
+	static Double monInt;
+	static Scanner keyboard = new Scanner(System.in);
+
+	public static void setInterest(){
+		System.out.println( "Hello! Please enter your annual interest rate." );
+		interest = keyboard.nextDouble();
+	}
+	
+	public static void setStartBal(){
+		System.out.println( "Now enter your account's starting balance." );
+		bal = keyboard.nextDouble();
+	}
+	
+	public static void setMonths(){
+		System.out.println( "Please enter the number of months since your account was opened." );
+		months = keyboard.nextDouble();
+	}
+	
+	public static void setWithdrawal(){
+		System.out.println( "Enter the withdrawal from your account this month." );
+		withdrawal = keyboard.nextDouble();
+		bal = -withdrawal;
+	}
+	
+	public static void setDeposit(){
+		System.out.println( "Enter the deposit to your account this month." );
+		deposit = keyboard.nextDouble();
+		bal = +deposit;
+	}
+	
+	public static void setMonInt(){
+		monInt = interest/12;
+		bal = +monInt;
+	}
+	
+	
+	
 }
--- src/SavingsAccountTest.java
+++ src/SavingsAccountTest.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /**
  * 
  */
@@ -15,25 +13,18 @@ public class SavingsAccountTest {
 	 * @param args The main arguments for the SavingsAccountTest class
 	 */
 	public static void main(String[] args) {
-
-		Double interest;
-		Double startBal;
-		Double months;
-		Double deposit;
-		Double withdrawal;
-		Double monInt;
-		Scanner keyboard = new Scanner( System.in );
 		
-		System.out.println( "Hello! Please enter your annual interest rate." );
-		interest = keyboard.nextDouble();
+		SavingsAccount.setInterest();
+		SavingsAccount.setStartBal();		
+		SavingsAccount.setMonths();
+		SavingsAccount.setWithdrawal();
+		SavingsAccount.setDeposit();
 		
-		System.out.println( "Now enter your account's starting balance." );
-		startBal = keyboard.nextDouble();
 		
-		System.out.println( "Please enter the number of months since your account was opened." );
-		months = keyboard.nextDouble();
 		
-		set
+		
+		
+	
 		
 	}
 
