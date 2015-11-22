b2c2b0fce4f6494553d80d9523132f24d98a5e2b
rydanie <rydanielson18@saintjosephhs.com>
2015-11-22 11:26:15 -0500

    there
    almost there

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,11 +1,62 @@
-/**
- * 
- */
 
-/**
- * @author woytek
- *
- */
-public class SavingsAccount {
+	/**
+	 * @author Ryley
+	 *
+	 */
+	public class SavingsAccount {
 
-}
+		private double intrate;
+		private double anualIntrate;
+		private double money;
+		private double intrestBalance;
+		private double newMoney;
+		
+		
+		public SavingsAccount(double balance){
+			
+			money = balance;
+			
+		}
+		
+		public void setMonthIntrest(double intrest){
+			
+			anualIntrate = intrest;
+			
+			intrate = anualIntrate/12;
+			
+			intrestBalance = money*intrate;
+			
+			money = intrestBalance + money;
+		}
+		
+		public double getMonthIntrest(){
+			
+			return money;
+		}
+		
+		public double getIntrest(){
+			return intrestBalance;
+		}
+		
+		public void setWithdraw(double withdraw){
+			
+			money = money - withdraw;
+		}
+		
+		public double getWithdraw(){
+			
+			return money;
+		}
+		
+		public void setDeposite(double deposite){
+			
+			money = money + deposite;
+		}
+		
+		public double getDeposite(){
+			
+			return money;
+		}
+		
+	}
+
--- src/SavingsAccountTest.java
+++ src/SavingsAccountTest.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -14,7 +16,69 @@ public class SavingsAccountTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int testChange;
+		int ctr;
+		int months = 0;
+		double balance = 0;
+		double totalIntrest = 0;
+		double withdraw;
+		double totalWithdraw = 0;
+		double deposit;
+		double totalDeposit = 0;
+		double totalBalance = 0;
+		final double yourIntrest;
+		
+		Scanner key = new Scanner(System.in);
+		
+		SavingsAccount count = new SavingsAccount(balance);
+		
+		System.out.println("What is you starting balance");
+		balance = key.nextDouble();
+		
+		System.out.println("What is your anual intrest rate");
+		yourIntrest = key.nextDouble();
+		
+		System.out.println("How many months have you had this account?");
+		months = key.nextInt();
+		
+		for(ctr=1; ctr<=months; ctr++){
+			
+			System.out.println("How much money was deposited during month " + ctr);
+			deposit = key.nextDouble();
+			count.setDeposite(deposit);
+			totalDeposit = totalDeposit + deposit;
+			
+			System.out.println(totalDeposit);
+			
+			System.out.println("How much money was withdrawn during month " + ctr);
+			withdraw = key.nextDouble();
+			count.setWithdraw(withdraw);
+			totalWithdraw = totalWithdraw + withdraw;
+			
+			System.out.println(totalWithdraw);
+			
+			count.setMonthIntrest(yourIntrest);
+			totalIntrest = totalIntrest + count.getIntrest();
+			
+			System.out.println(totalIntrest);
+			
+			totalBalance = totalBalance + count.getMonthIntrest();
+			
+			System.out.println(totalBalance);
+		}
+		
+		System.out.println("\nYour total balance after " + months + " is " 
+				           + totalBalance);
+		
+		System.out.println("You deposited " + totalDeposit + " dolars.");
+		System.out.println("You withdrew " + totalWithdraw + " dolars.");
+		System.out.println("You collected " + totalIntrest + " dolars.");
+		
+		
+		
+		
+	
+		
+		
 
 	}
 
