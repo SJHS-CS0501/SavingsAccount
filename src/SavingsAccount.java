0364039f64b41c00a21516392dae5a97e82a88ea
Jack Protivnak <jprotivnak@me.com>
2015-11-22 20:55:54 -0500

    Finished
    Finished all work and created JavaDoc

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,18 +1,67 @@
+import java.util.Scanner;
+
 /**
  * @author Jack Protivnak
- *
  */
 public class SavingsAccount{
-	private double startBal;
-	private double interest;
+	static Scanner keyboard = new Scanner(System.in); // Notifying the system that the keyboard will be used as input
+	private static double interest = 0, startBalance = 0, balance = 0, months = 0, deposit = 0, withdraw = 0, interestTotal = 0, methodInterest = 0; // Declaring double private variables
+	private static int depCount = 0, withCount = 0; // Declaring private integer variables
 	
+	/**
+	 * Constructor and setting startBalance and balance
+	 * @param bal
+	 */
 	public SavingsAccount( double bal ) {
-		startBal = bal;
+		startBalance = bal;
+		balance = bal;
 	}
 	
-	public void Interest() {
-		System.out;println( "Please enter your annual interest as a percent: " );
-		interest = nextDouble();
+	/**
+	 * Adding deposits to balance
+	 * @param dep
+	 * @return
+	 */
+	public static double balanceAdd( double dep ) {
+		balance += dep;
+		return balance;
+	}
+	
+	/**
+	 * Subtracting withdraws from balance
+	 * @param with
+	 * @return
+	 */
+	public static double balanceSubtract( double with ) {
+		balance -= with;
+		return balance;
+	}
+	
+	/**
+	 * Interest function to determine the interest on the balance per month
+	 * @param inter
+	 * @return
+	 */
+	public static double interest( double inter) {
+		methodInterest = ((inter/12)/100);
+		interestTotal += (balance*methodInterest);
+		balance += interestTotal;
+		return balance;
+	}
+	
+	/**
+	 * Fetches balance
+	 * @return
+	 */
+	public static double getBalance() {
+		return balance;
+	}
+	
+	/**
+	 * Fetches total interest earned
+	 * @return
+	 */
+	public static double getInterestTotal() {
+		return interestTotal;
 	}
 }
-
--- src/SavingsAccountTest.java
+++ src/SavingsAccountTest.java
@@ -1,19 +1,65 @@
-/**
- * 
- */
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 /**
- * @author woytek
- *
+ * @author Jack Protivnak
  */
 public class SavingsAccountTest {
 
 	/**
+	 * This program is designed to have a user input their bank account's starting
+	 * balance and then ask for their annual interest rate, how many months had passed
+	 * since they opened the account, and how much money was put into and removed from 
+	 * the account. It will then tell them their ending balance at the end of the period
+	 * and tell them their interest earned with how many times they deposited
+	 * and withdrew.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner keyboard = new Scanner(System.in); // Notifying the system that the keyboard will be used as input
+		DecimalFormat formatter =  new DecimalFormat( "########.####" ); // Defining format for displayed numbers in program.
+		double startingBalance, interest, months, deposit, withdraw; // Declaring double variables
+		int depCount = 0, withCount = 0; // Declaring integer variables
+	
+		// User to enter their starting balance in account
+		System.out.println( "Please enter your starting balance: " );
+		startingBalance = keyboard.nextDouble();
+		SavingsAccount user = new SavingsAccount( startingBalance );
+		
+		// User to enter their annual interest rate 
+		System.out.println( "Please enter your annual interest as a percent: " );
+		interest = keyboard.nextDouble();
+		
+		// User to enter how many months had passed since they opened their account
+		System.out.println( "Please enter the number of months that have passed since the account was created: " );
+		months = keyboard.nextDouble();
+		
+		// A for loop for each month to see if any deposits or withdraws
+		for (int ctr = 0; ctr < months; ctr++) {
+			System.out.println( "Please enter how much money was deposited into the account during month " + (ctr + 1) + ": " );
+			deposit = keyboard.nextDouble();			
+			user.balanceAdd(deposit);
+			if (deposit > 0 ) {
+				depCount++;
+			}
+			
+			System.out.println( "Please enter how much money was withdrawn from the account during month " + (ctr + 1) + ": " );
+			withdraw = keyboard.nextDouble();
+			user.balanceSubtract(withdraw);
+			if( withdraw > 0 ) { //Regular Expression
+				withCount++;
+				}
+			user.interest( interest );
+			}
+		
+		
+		
+			// Telling user their results
+			System.out.println( "Your ending balance is: $" + formatter.format(user.getBalance()) );
+			System.out.println( "You deposited money " + depCount + " times." );
+			System.out.println( "You withdrew money " + withCount + " times." );
+			System.out.println( "You earned $" + formatter.format(user.getInterestTotal()) + " in interest.");
 	}
 
 }
