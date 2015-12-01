8fb0e3b76b45e64bf765d9562e43e44e634f73f2
isschroeder <kittypuppybird@gmail.com>
2015-11-30 19:47:40 -0500

    Finally here!
    Got github to like me again. This should be the full program all fixed
    and happy.

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
 /**
- * 
- */
-
-/**
  * @author woytek
  * @author Isabelle Schroeder
- *
+ * This class holds all the methods for the SavingsAccount program.
  */
-public class SavingsAccount {
-
-	private static Double interest;
-	private static Double bal;
-	private static Double months;
-	static Double deposit;
-	static Double withdrawal;
-	static Double monInt;
+ public class SavingsAccount {
+ 
+	private static Double interest = 0.0;
+	private static Double bal = 0.0;
+	private static Double months = 0.0;
+	static Double deposit = 0.0;
+	static Double withdrawal = 0.0;
+	static Double monInt = 0.0;
 	static Scanner keyboard = new Scanner(System.in);
+	static Double outMoney = 0.0;
+	static Double inMoney = 0.0;
 
 	public static void setInterest(){
 		System.out.println( "Hello! Please enter your annual interest rate." );
@@ -34,23 +32,43 @@ public class SavingsAccount {
 		months = keyboard.nextDouble();
 	}
 	
-	public static void setWithdrawal(){
-		System.out.println( "Enter the withdrawal from your account this month." );
-		withdrawal = keyboard.nextDouble();
-		bal = -withdrawal;
+	public static double getMonths(){
+		return months;
 	}
+	
+	public static void setWithdrawal(){
+			System.out.println( "Enter the withdrawal from your account this month." );
+			withdrawal = keyboard.nextDouble();
+			bal -= withdrawal;
+			outMoney += withdrawal;
+			}
 	
 	public static void setDeposit(){
 		System.out.println( "Enter the deposit to your account this month." );
 		deposit = keyboard.nextDouble();
-		bal = +deposit;
+		bal += deposit;
+		inMoney += deposit;
 	}
 	
 	public static void setMonInt(){
 		monInt = interest/12;
-		bal = +monInt;
+		bal += bal*monInt;
 	}
 	
+	public static double getWithdrawals(){
+		return outMoney;
+	}
 	
+	public static double getDeposits(){
+		return inMoney;
+	}
 	
-}
+	public static double getMonInt(){
+		return monInt;
+	}
+	
+	public static double getBalance(){
+		 return bal;
+	 }
+	
+ }
--- src/SavingsAccountTest.java
+++ src/SavingsAccountTest.java
@@ -1,11 +1,8 @@
 /**
- * 
- */
-
-/**
- * This program will calculate your savings account value!
  * @author woytek
  * @author Isabelle Schroeder
+ * This program will tell you your account balance!
+ * 
  */
 public class SavingsAccountTest {
 
@@ -13,19 +10,37 @@ public class SavingsAccountTest {
 	 * @param args The main arguments for the SavingsAccountTest class
 	 */
 	public static void main(String[] args) {
-		
+		int ct = 1;
+		double  months = 0;
+		double balance = 0;
+		double deposits = 0;
+		double withdrawals = 0;
+
 		SavingsAccount.setInterest();
 		SavingsAccount.setStartBal();		
 		SavingsAccount.setMonths();
-		SavingsAccount.setWithdrawal();
-		SavingsAccount.setDeposit();
+		
+		months = SavingsAccount.getMonths();
+		
+		System.out.println( "The program will now ask you questions "
+				+ "regarding each month since you've had your account." );
+		
+		for( ct = 1; ct <= months; ct++){
+			SavingsAccount.setWithdrawal();
+			SavingsAccount.setDeposit();
+			SavingsAccount.setMonInt();
+			
+		}
+		
+		balance = SavingsAccount.getBalance();
+		withdrawals = SavingsAccount.getWithdrawals();
+		deposits = SavingsAccount.getDeposits();
 		
 		
-		
-		
-		
-	
-		
+		System.out.printf("Total deposits: %,.2f \n", deposits );
+		System.out.printf("Toal withdrawals: %,.2f \n", withdrawals);
+		System.out.println("Total interest: " + SavingsAccount.getMonInt() );
+		System.out.printf("Balance: %,.2f \n", balance );
 	}
 
 }
