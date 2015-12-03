f181a7bc2ea8e3bae88c7fb5dec1706867823825
isschroeder <kittypuppybird@gmail.com>
2015-12-02 22:08:01 -0500

    Done for realsies
    added comments and made some happy changes

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -13,29 +13,34 @@ import java.util.Scanner;
 	static Double deposit = 0.0;
 	static Double withdrawal = 0.0;
 	static Double monInt = 0.0;
-	static Scanner keyboard = new Scanner(System.in);
-	static Double outMoney = 0.0;
-	static Double inMoney = 0.0;
+	static Scanner keyboard = new Scanner(System.in); // so user can type stuff
+	static Double outMoney = 0.0; // for total withdrawals
+	static Double inMoney = 0.0; // for total deposits
 
+	// getting user's annual interest rate
 	public static void setInterest(){
 		System.out.println( "Hello! Please enter your annual interest rate." );
 		interest = keyboard.nextDouble();
 	}
 	
+	// getting user's starting balance
 	public static void setStartBal(){
 		System.out.println( "Now enter your account's starting balance." );
 		bal = keyboard.nextDouble();
 	}
 	
+	// getting months since the user opened their account
 	public static void setMonths(){
 		System.out.println( "Please enter the number of months since your account was opened." );
 		months = keyboard.nextDouble();
 	}
 	
+	// returning months to SavingsAccountTest class for the for loop
 	public static double getMonths(){
 		return months;
 	}
 	
+	// subtracting withdrawal from account total (per month)
 	public static void setWithdrawal(){
 			System.out.println( "Enter the withdrawal from your account this month." );
 			withdrawal = keyboard.nextDouble();
@@ -43,6 +48,7 @@ import java.util.Scanner;
 			outMoney += withdrawal;
 			}
 	
+	// adding deposit to account total (per month)
 	public static void setDeposit(){
 		System.out.println( "Enter the deposit to your account this month." );
 		deposit = keyboard.nextDouble();
@@ -50,23 +56,28 @@ import java.util.Scanner;
 		inMoney += deposit;
 	}
 	
+	// calculating monthly interest using annual interest
 	public static void setMonInt(){
 		monInt = interest/12;
 		bal += bal*monInt;
 	}
 	
+	// returning total withdrawals for display
 	public static double getWithdrawals(){
 		return outMoney;
 	}
 	
+	// returning total deposits for display
 	public static double getDeposits(){
 		return inMoney;
 	}
 	
+	// returning monthly interest earned for display
 	public static double getMonInt(){
 		return monInt;
 	}
 	
+	// returning final balance for display
 	public static double getBalance(){
 		 return bal;
 	 }
--- src/SavingsAccountTest.java
+++ src/SavingsAccountTest.java
@@ -10,37 +10,33 @@ public class SavingsAccountTest {
 	 * @param args The main arguments for the SavingsAccountTest class
 	 */
 	public static void main(String[] args) {
-		int ct = 1;
-		double  months = 0;
-		double balance = 0;
-		double deposits = 0;
-		double withdrawals = 0;
+		int ct = 1; // used in for loop to continue execution until 
 
-		SavingsAccount.setInterest();
+		SavingsAccount.setInterest(); // to methods in SavingAccount class
 		SavingsAccount.setStartBal();		
 		SavingsAccount.setMonths();
-		
-		months = SavingsAccount.getMonths();
 		
 		System.out.println( "The program will now ask you questions "
 				+ "regarding each month since you've had your account." );
 		
-		for( ct = 1; ct <= months; ct++){
+		// loop continues to iterate for every month since the account was opened
+		for( ct = 1; ct <= SavingsAccount.getMonths(); ct++){
 			SavingsAccount.setWithdrawal();
 			SavingsAccount.setDeposit();
 			SavingsAccount.setMonInt();
 			
 		}
 		
-		balance = SavingsAccount.getBalance();
-		withdrawals = SavingsAccount.getWithdrawals();
-		deposits = SavingsAccount.getDeposits();
+		/*
+		 * displaying total deposits, withdrawals, interest, and final balance. Monies displayed 
+		 * to two decimal places and interest to five places because it will probably be tiny unless 
+		 * the user has gone to a really spiffy bank for a long time (or they're super rich).
+		 */
 		
-		
-		System.out.printf("Total deposits: %,.2f \n", deposits );
-		System.out.printf("Toal withdrawals: %,.2f \n", withdrawals);
-		System.out.println("Total interest: " + SavingsAccount.getMonInt() );
-		System.out.printf("Balance: %,.2f \n", balance );
+		System.out.printf("Total deposits: %,.2f \n", SavingsAccount.getDeposits() );
+		System.out.printf("Total withdrawals: %,.2f \n", SavingsAccount.getWithdrawals() );
+		System.out.printf("Total interest: %,.5f \n" + SavingsAccount.getMonInt() );
+		System.out.printf("Balance: %,.2f \n", SavingsAccount.getBalance() );
 	}
 
 }
